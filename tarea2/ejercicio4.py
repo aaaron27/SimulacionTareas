579699d751582471260aca114c10a7f5835c24a9
aaaron27 <a.perez.1@estudiantec.cr>
2025-12-23 20:27:06 -0600

    checked ejercicio4.py

--- tarea2/ejercicio4.py
+++ tarea2/ejercicio4.py
@@ -10,18 +10,23 @@ def esCompleto(list):
     return None
 
 def todo(x0, a, c, m, l):
-    list = [0 for _ in range(m)]
-    list[x0] = 1
+    vis = [0 for _ in range(m)]
+    vis[x0] = 1
 
     print(f"{l}:")
     for _ in range(m+1):
         print(x0,end=" ")
         x0 = congMixto(x0, a, c, m)
-        list[x0] = 1
+        vis[x0] = 1
     print()
     
-    val = esCompleto(list)
-    print("\nEs completo?", "Si" if val == None else f"Falta el numero {val} es aparecer\n")
+    faltantes = []
+    for i in range(1, len(vis)):
+        if not vis[i]:
+            faltantes.append(i)
+
+    print("Es completo?", "Si" if not len(faltantes) else f"Faltan los numeros {faltantes} en aparecer")
+    print()
     
 todo(7, 5, 24, 32, 'a')
 todo(8, 9, 13, 32, 'b')
