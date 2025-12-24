f5973ebc3cb2865732f79fe5ef4bc1bfdd656b71
aaaron27 <a.perez.1@estudiantec.cr>
2025-12-23 20:21:20 -0600

    checked ejercicio3.py

--- tarea2/ejercicio3.py
+++ tarea2/ejercicio3.py
@@ -11,18 +11,11 @@ def cuadradosMedios(n):
 	return (n//(10**((cd-DIGITOS)>>1)))%10**DIGITOS
 
 def todo(x):
-    freq = {}
     print(f"x(0) = {x}")
-    freq[x] = freq.get(x,0) + 1
     for _ in range(50):
         x = cuadradosMedios(x)
         print(x,end=" ")
-        freq[x] = freq.get(x,0) + 1
-    print()
-    print("\nFrecuencias:")
-    for k, v in freq.items():
-        print(f"\t{k}: {v}")
-    print()
+    print("\n")
 
 todo(3567345)
 todo(1234500012)
