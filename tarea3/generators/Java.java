f020a124864713dffbf1972dae2d8c56dd9c05bf
aaaron27 <a.perez.1@estudiantec.cr>
2026-01-05 16:45:49 -0600

    update Java.java

--- tarea3/generators/Java.java
+++ tarea3/generators/Java.java
@@ -1,5 +1,25 @@
-public class Java {
+import java.util.Random;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public final class Java {
+    private static final String FILE_PATH = "../data/Java.txt";
+    private static final int N = 1000000;
+
+    private Java() {}
+
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        try (FileWriter fw = new FileWriter(FILE_PATH)) {
+            final Random random = new Random();
+           
+            for (int i = 0; i < N; i++) {
+                fw.write(Double.toString(random.nextDouble()));
+                fw.write('\n');
+            }
+
+            fw.close(); 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
